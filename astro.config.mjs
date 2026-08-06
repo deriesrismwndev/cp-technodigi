97ca189039c7477cc8d55d72481cb37ba46f194d
deries <deries@L-DERIES-ITDEV.local>
2026-08-06 20:58:18 +0700

    adjusment seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: 'https://technodigi.co.id',
+  integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
+
